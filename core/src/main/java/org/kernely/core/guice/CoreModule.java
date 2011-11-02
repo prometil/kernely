a79bfff2c33d396e099b5af367b328592c21882e
Julien Chakra-Breil <julien.chakra@gmail.com>
2011-11-02 15:21:24 +0100

    Adds headers copyright

--- core/src/main/java/org/kernely/core/guice/CoreModule.java
+++ core/src/main/java/org/kernely/core/guice/CoreModule.java
@@ -1,6 +1,22 @@
-/**
- * 
- */
+/*
+Copyright 2011 Prometil SARL
+
+This file is part of Kernely.
+
+Kernely is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Kernely is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with Kernely.
+If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.kernely.core.guice;
 
 import groovy.text.SimpleTemplateEngine;
@@ -15,7 +31,6 @@ import com.google.inject.AbstractModule;
 
 /**
  * The core module
- * @author g.breton
  *
  */
 public class CoreModule extends AbstractModule {
