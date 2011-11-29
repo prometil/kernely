87fe0d7e7c4620e2839efd7c48c05efe0d410819
Guillaume Breton <breton.gy@gmail.com>
2011-11-29 22:11:09 +0100

    Adds security to AbstractServiceTest renames getCurrent to getAuthenticatedUser() and tests getAuthenticatedUser()

--- core/src/main/java/org/kernely/core/resources/UserController.java
+++ core/src/main/java/org/kernely/core/resources/UserController.java
@@ -16,7 +16,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public
 License along with Kernely.
 If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package org.kernely.core.resources;
 
 import java.io.File;
@@ -49,45 +49,43 @@ import com.google.inject.Inject;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
-
 @Path("/user")
-public class UserController  extends AbstractController{
-	
+public class UserController extends AbstractController {
+
 	@Inject
 	private AbstractConfiguration configuration;
-	
 
 	@Inject
 	private TemplateRenderer templateRenderer;
-	
+
 	@Inject
 	private UserService userService;
 
 	/**
 	 * Display the list of users.
+	 * 
 	 * @return The html content to display the list.
 	 */
 	@GET
 	@Produces( { MediaType.TEXT_HTML })
-	public String getText()
-	{
+	public String getText() {
 		log.debug("Call to GET on all users");
 		List<UserDTO> users = userService.getAllUsers();
-		return templateRenderer.create("/templates/gsp/users.gsp").with("users", users).render() ;
+		return templateRenderer.create("/templates/gsp/users.gsp").with("users", users).render();
 	}
 
 	/**
 	 * Create a new user with a random username and as password : "password".
+	 * 
 	 * @return "Ok", not useful.
 	 */
 	@GET
 	@Path("/create")
 	@Produces( { MediaType.TEXT_PLAIN })
-	public String create()
-	{
+	public String create() {
 		log.debug("Create a user");
 		UserCreationRequestDTO request = new UserCreationRequestDTO();
-		request.username ="user";
+		request.username = "user";
 		request.password = "password";
 		userService.createUser(request);
 		return "User created";
@@ -95,6 +93,7 @@ public class UserController  extends AbstractController{
 
 	/**
 	 * Displays the login page.
+	 * 
 	 * @return The html content of the login page.
 	 */
 	@GET
@@ -106,6 +105,7 @@ public class UserController  extends AbstractController{
 
 	/**
 	 * Displays the login page.
+	 * 
 	 * @return The html content of the login page.
 	 */
 	@POST
@@ -113,12 +113,13 @@ public class UserController  extends AbstractController{
 	@Produces( { MediaType.TEXT_HTML })
 	public String postLogin() {
 		log.info("Login attempt : is authenticated {}", SecurityUtils.getSubject().isAuthenticated());
-		
+
 		return templateRenderer.create("/templates/gsp/login.gsp").withoutLayout().render();
 	}
-	
+
 	/**
 	 * Logs out the user and redirect to the main page.
+	 * 
 	 * @return a redirection to the main page;
 	 */
 	@GET
@@ -129,114 +130,122 @@ public class UserController  extends AbstractController{
 		SecurityUtils.getSubject().logout();
 		return redirect("/");
 	}
-	
-	
+
 	/**
 	 * Display the profile of the user who has the specified login
-	 * @param userLogin The login of the user that profile is needed
+	 * 
+	 * @param userLogin
+	 *            The login of the user that profile is needed
 	 * @return the profile page fill with associated informations
 	 */
 	@GET
 	@Path("/{login}/profile")
 	@Produces( { MediaType.TEXT_HTML })
-	public String profil(@PathParam("login")String userLogin) {
+	public String profil(@PathParam("login") String userLogin) {
 		String template = "/templates/gsp/profile.gsp";
 		UserDTO usercurrent = this.getCurrent();
-		if(usercurrent.username.equals(userLogin)){
+		if (usercurrent.username.equals(userLogin)) {
 			template = "/templates/gsp/profile_editable.gsp";
 		}
 		UserDetailsDTO uddto = userService.getUserDetails(userLogin);
 		String imagePath = "/images/default_user.png";
-		if(uddto.image != null && uddto.image.equals("null"))
-		{
-			uddto.image=null;
+		if (uddto.image != null && uddto.image.equals("null")) {
+			uddto.image = null;
 		}
-		if(uddto.image != null){
+		if (uddto.image != null) {
 			imagePath = "/images/" + uddto.image;
 		}
-		
-		return templateRenderer.create(template).with("username", uddto.firstname + " " + uddto.lastname).with("lastname", uddto.lastname).with("firstname", uddto.firstname).with("email", uddto.email).with("image", imagePath).with("imagename", uddto.image).with("adress", uddto.adress).with("zip", uddto.zip).with("city", uddto.city).with("homephone", uddto.homephone).with("mobilephone", uddto.mobilephone).with("businessphone", uddto.businessphone).with("birth", uddto.birth).with("nationality", uddto.nationality).with("ssn", uddto.ssn).with("civility", uddto.civility).render();
+
+		return templateRenderer.create(template).with("username", uddto.firstname + " " + uddto.lastname).with("lastname", uddto.lastname).with("firstname", uddto.firstname).with("email", uddto.email).with("image", imagePath).with("imagename", uddto.image).with("adress", uddto.adress).with("zip",
+				uddto.zip).with("city", uddto.city).with("homephone", uddto.homephone).with("mobilephone", uddto.mobilephone).with("businessphone", uddto.businessphone).with("birth", uddto.birth).with("nationality", uddto.nationality).with("ssn", uddto.ssn).with("civility", uddto.civility).render();
 	}
-	
 
 	/**
-	 * Edit the profile of the current user with the new informations contained in the UserDetailsUpdateRequestDTO user
-	 * @param user the DTO which contained all new informations about the user to update
+	 * Edit the profile of the current user with the new informations contained
+	 * in the UserDetailsUpdateRequestDTO user
+	 * 
+	 * @param user
+	 *            the DTO which contained all new informations about the user to
+	 *            update
 	 * @return the DTO associated to the user updated
 	 */
 	@POST
 	@Path("/{login}/profile/update")
 	public UserDetailsDTO editProfil(UserDetailsUpdateRequestDTO user) {
-			
+
 		UserDetailsDTO ud = userService.getUserDetails(userService.getCurrentUser().username);
-		UserDetailsDTO uddto = new UserDetailsDTO(user.firstname, user.lastname, user.image,user.email, user.adress,user.zip, user.city, user.homephone, user.mobilephone,user.businessphone, user.birth, user.nationality, user.ssn,user.civility,ud.id, new UserDTO()); 
-		
-		//Match the user id (foreign key) with the userdetailid
-		user.id=ud.id;
+		UserDetailsDTO uddto = new UserDetailsDTO(user.firstname, user.lastname, user.image, user.email, user.adress, user.zip, user.city, user.homephone, user.mobilephone, user.businessphone, user.birth, user.nationality, user.ssn, user.civility, ud.id, new UserDTO());
+
+		// Match the user id (foreign key) with the userdetailid
+		user.userDetailsId = ud.id;
 		// Call UserService to update informations
 		userService.updateUserProfile(user);
 		return uddto;
 	}
-	
+
 	/**
 	 * Upload a specified file
-	 * @param uploadedInputStream the InputStream corresponding to the file uploaded
-	 * @param fileDetail the informations about file uploaded
+	 * 
+	 * @param uploadedInputStream
+	 *            the InputStream corresponding to the file uploaded
+	 * @param fileDetail
+	 *            the informations about file uploaded
 	 */
 	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public void uploadFile(
-			@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail) {
-		if (fileDetail.getFileName().equals("")){
-			return ; 
+	public void uploadFile(@FormDataParam("file") InputStream uploadedInputStream, @FormDataParam("file") FormDataContentDisposition fileDetail) {
+		if (fileDetail.getFileName().equals("")) {
+			return;
 		}
-		//get extension
-		String[] extension= fileDetail.getFileName().split("\\.");
-		if (extension.length<2) {
+		// get extension
+		String[] extension = fileDetail.getFileName().split("\\.");
+		if (extension.length < 2) {
 			throw new IllegalArgumentException("the file need an extansion");
 		}
-		
+
 		SecureRandom random = new SecureRandom();
-		String fileName = new BigInteger(130, random).toString(32)+ "." + extension[extension.length - 1];
+		String fileName = new BigInteger(130, random).toString(32) + "." + extension[extension.length - 1];
 		String prefix = configuration.getString("workpath.url");
-		String uploadedFileLocation = prefix + "/images/" + fileName ; ///core/src/main/resources
+		String uploadedFileLocation = prefix + "/images/" + fileName; // /core/src/main/resources
 		try {
 			FileUtils.copyInputStreamToFile(uploadedInputStream, new File(uploadedFileLocation));
 		} catch (IOException e) {
 			log.debug(e.getMessage());
 		}
-		
-		// up to date the database		
-		UserDetailsDTO user= userService.getUserDetails(userService.getCurrentUser().username);
-		
-		UserDetailsUpdateRequestDTO ud = new UserDetailsUpdateRequestDTO(user.firstname, user.lastname, fileName,user.email, user.adress,user.zip, user.city, user.homephone, user.mobilephone,user.businessphone, user.birth, user.nationality, user.ssn, user.id ,user.civility);
+
+		// up to date the database
+		UserDetailsDTO user = userService.getUserDetails(userService.getCurrentUser().username);
+
+		UserDetailsUpdateRequestDTO ud = new UserDetailsUpdateRequestDTO(user.firstname, user.lastname, fileName, user.email, user.adress, user.zip, user.city, user.homephone, user.mobilephone, user.businessphone, user.birth, user.nationality, user.ssn, user.id, user.civility);
 		userService.updateUserProfile(ud);
-	
+
 	}
-	
+
 	/**
 	 * Get the DTO associated to the current user
+	 * 
 	 * @return the DTO associated to the current user
 	 */
 	@GET
 	@Path("/current")
-	@Produces({"application/json"})
-	public UserDTO getCurrent(){
+	@Produces( { "application/json" })
+	public UserDTO getCurrent() {
 		return userService.getCurrentUser();
 	}
-	
+
 	/**
 	 * Get the DTO associated to the specified user who has the login 'login'
-	 * @param userLogin the login of the needed user
+	 * 
+	 * @param userLogin
+	 *            the login of the needed user
 	 * @return the DTO associated to the specified user
 	 */
 	@GET
 	@Path("/{login}")
-	@Produces({"application/json"})
-	public UserDetailsDTO getDetails(@PathParam("login")String userLogin){
+	@Produces( { "application/json" })
+	public UserDetailsDTO getDetails(@PathParam("login") String userLogin) {
 		return userService.getUserDetails(userLogin);
-		
+
 	}
 }
--- core/src/test/java/org/kernely/core/common/AbstractServiceTest.java
+++ core/src/test/java/org/kernely/core/common/AbstractServiceTest.java
@@ -21,10 +21,22 @@ package org.kernely.core.common;
 
 import javax.persistence.EntityManager;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.UnavailableSecurityManagerException;
+import org.apache.shiro.util.LifecycleUtils;
+import org.apache.shiro.util.ThreadState;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
+import org.kernely.core.model.User;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.support.SubjectThreadState;
+import org.apache.shiro.util.LifecycleUtils;
+import org.apache.shiro.util.ThreadState;
 import com.google.guiceberry.junit4.GuiceBerryRule;
 import com.google.inject.Inject;
 import com.google.inject.persist.PersistService;
@@ -35,34 +47,94 @@ import com.google.inject.persist.PersistService;
  */
 public abstract class AbstractServiceTest {
 
-
 	@Rule
 	public final GuiceBerryRule guiceBerry = new GuiceBerryRule(TestModule.class);
 
 	@Inject
 	Initializer initializer;
-	
+
 	@Inject
 	EntityManager em;
 
 	@Inject
 	PersistService service;
 
+	private static ThreadState subjectThreadState;
+
 	@Before
 	public void openTransactionBefor() {
 		// service.start();
-		if(!em.getTransaction().isActive()){
+		if (!em.getTransaction().isActive()) {
 			em.getTransaction().begin();
 		}
-		 
 
 	}
 
 	@After
 	public void closeTransactionAfter() {
 		// service.stop();
-		if(em.getTransaction().isActive()){
+		if (em.getTransaction().isActive()) {
 			em.getTransaction().rollback();
 		}
+		clearSubject();
+	}
+
+	private static void doClearSubject() {
+		if (subjectThreadState != null) {
+			subjectThreadState.clear();
+			subjectThreadState = null;
+		}
+	}
+
+	/**
+	 * Allows subclasses to set the currently executing {@link Subject}
+	 * instance.
+	 * 
+	 * @param subject
+	 *            the Subject instance
+	 */
+	protected void setSubject(Subject subject) {
+		clearSubject();
+		subjectThreadState = createThreadState(subject);
+		subjectThreadState.bind();
+	}
+
+	protected void clearSubject() {
+		doClearSubject();
+	}
+
+	protected Subject getSubject() {
+		return SecurityUtils.getSubject();
+	}
+
+	protected ThreadState createThreadState(Subject subject) {
+		return new SubjectThreadState(subject);
+	}
+
+	protected static void setSecurityManager(SecurityManager securityManager) {
+		SecurityUtils.setSecurityManager(securityManager);
+	}
+
+	protected static SecurityManager getSecurityManager() {
+		return SecurityUtils.getSecurityManager();
+	}
+
+	@AfterClass
+	public static void tearDownShiro() {
+		doClearSubject();
+		try {
+			SecurityManager securityManager = getSecurityManager();
+			LifecycleUtils.destroy(securityManager);
+		} catch (UnavailableSecurityManagerException e) {
+			// we don't care about this when cleaning up the test environment
+			// (for example, maybe the subclass is a unit test and it didn't
+			// need a SecurityManager instance because it was using only
+			// mock Subject instances)
+		}
+		setSecurityManager(null);
+	}
+
+	public void authenticatedAs(User user) {
+
 	}
 }
