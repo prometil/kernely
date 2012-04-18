57a86c61f8a4254ef29ce921434b8995afb0ce7d
Jerome Barthas <jerome.barthas@gmail.com>
2012-04-18 12:05:18 +0200

    Change the layout to consider left and right sides bars. Transforms admin main page to a sub-page of main layout.

--- core/src/main/java/org/kernely/core/controller/AdminController.java
+++ core/src/main/java/org/kernely/core/controller/AdminController.java
@@ -20,7 +20,9 @@
 package org.kernely.core.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -32,7 +34,7 @@ import org.kernely.core.dto.PluginDTO;
 import org.kernely.core.plugin.AbstractPlugin;
 import org.kernely.core.plugin.PluginsLoader;
 import org.kernely.core.service.user.UserService;
-import org.kernely.core.template.TemplateRenderer;
+import org.kernely.core.template.SobaTemplateRenderer;
 
 import com.google.inject.Inject;
 
@@ -43,7 +45,7 @@ import com.google.inject.Inject;
 public class AdminController extends AbstractController {
 
 	@Inject
-	private TemplateRenderer templateRenderer;
+	private SobaTemplateRenderer templateRenderer;
 
 	@Inject
 	private UserService userService;
@@ -61,11 +63,13 @@ public class AdminController extends AbstractController {
 	public Response getAdmin() {
 		javax.ws.rs.core.Response page;
 
+		Map<String, Object> map =new HashMap<String, Object>();
+		
 		// Display the admin page only if the user is admin.
 		if (userService.currentUserIsAdministrator()) {
-			page = ok(templateRenderer.create("/templates/gsp/admin.gsp").with("extension", "").addCss("/css/admin.css"));
+			page = Response.ok(templateRenderer.render("templates/admin.html", map)).build();
 		} else {
-			page = ok(templateRenderer.create("/templates/gsp/home.gsp"));
+			page = Response.ok(templateRenderer.render("templates/home.html",map)).build();
 		}
 		return page;
 	}
